eae0ce497556ac9a178f0a63372c77d23e2e8aa8
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-10-02 18:33:05 -0700

    working on the jump mechanic for the player

---
+++ Assets/Scripts/JumpButton/JumpButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JumpButtonScript : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+
+    public void OnPointerDown(PointerEventData data){
+        if(PlayerJumpScript.instance != null){
+            PlayerJumpScript.instance.SetPower(true);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData data){
+        PlayerJumpScript.instance.SetPower(false);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerJumpScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpScript : MonoBehaviour
+{
+    public static PlayerJumpScript instance;
+
+    private Rigidbody2D rbody;
+    private Animator anim;
+    private float forceX, forceY;
+    private float thresholdX = 7f;
+    private float thresholdY = 14f;
+    private bool setPower, didJump;
+
+    private void Awake() {
+        MakeInstance();
+    }
+
+    /** 
+        This function creates a singleton that only exists within the current
+        scene.
+    */
+    void MakeInstance(){
+        if(instance == null){
+            instance = this;
+        }
+    }
+
+    public void SetPower(bool SetPower){
+        setPower = SetPower;
+        if(setPower){
+            print("we are setting the power");
+        }else{
+            print("not setting power");
+        }
+    }
+}
