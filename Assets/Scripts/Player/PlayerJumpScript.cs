b8b1f2efb4c70aebd401bfd49793ca693135e42d
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-10-02 18:57:00 -0700

    added mechanic to set power, jump and detect collision with platform

--- Assets/Scripts/Player/PlayerJumpScript.cs
+++ Assets/Scripts/Player/PlayerJumpScript.cs
@@ -8,6 +8,7 @@ public class PlayerJumpScript : MonoBehaviour
 
     private Rigidbody2D rbody;
     private Animator anim;
+    [SerializeField]
     private float forceX, forceY;
     private float thresholdX = 7f;
     private float thresholdY = 14f;
@@ -15,6 +16,12 @@ public class PlayerJumpScript : MonoBehaviour
 
     private void Awake() {
         MakeInstance();
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        SetPower();
     }
 
     /** 
@@ -27,12 +34,61 @@ public class PlayerJumpScript : MonoBehaviour
         }
     }
 
+    /** 
+        This function will increment the x and y force when setPower is true.
+        The max x and y force is also kept here as well. This funciton will
+        get called in update.
+    */
+    void SetPower(){
+        if(setPower){
+            forceX += thresholdX * Time.deltaTime;
+            forceY += thresholdY * Time.deltaTime;
+
+            if(forceX > 6.5f){
+                forceX = 6.5f;
+            }
+            if(forceY > 13.5f){
+                forceY = 13.5f;
+            }
+        }
+    }
+
+    /** 
+        This function will determine whether or not the power should continue
+        to be set. Once setpower is set to false, the player will jump when
+        didJump is also false to prevent double jumps.
+
+        @param {bool} Boolean that decided whether or not force should be incremented
+    */
     public void SetPower(bool SetPower){
         setPower = SetPower;
-        if(setPower){
-            print("we are setting the power");
-        }else{
-            print("not setting power");
+        if(!setPower && !didJump){
+            Jump();
+        }
+    }
+
+    /** 
+        This function will set the gameobjects velocity using the forcex and y.
+        Will also reset the forces and set didJump to true.
+    */
+    void Jump(){
+        rbody.velocity = new Vector2(forceX, forceY);
+        forceX = forceY = 0;
+        didJump = true;
+    }
+
+    /** 
+        This fucntion will detect collision when the player collides with a 
+        platform only when didJump is true.
+
+        @param {Collider2D} The other Collider2D involved in this collision.
+    */
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(didJump){
+            didJump = false;
+            if(other.tag == "Platform"){
+
+            }
         }
     }
 }
