a17be24415abeb17b8c14696f3a2c537487e7b6e
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-10-09 20:59:12 -0700

    added the game over manager script

---
+++ Assets/Scripts/GameManager/GameOverManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameOverManager instance;
+
+    [SerializeField]
+    private GameObject gameOverPanel;
+    private Animator gameoverAnim;
+    [SerializeField]
+    private Button playAgainBtn, backBtn;
+    [SerializeField]
+    private Text finalScore;
+    [SerializeField]
+    private GameObject scoreText;
+
+    private void Awake() {
+        gameoverAnim = gameOverPanel.GetComponent<Animator>();
+        playAgainBtn.onClick.AddListener(() => PlayAgain());
+        backBtn.onClick.AddListener(() => BackToMenu());
+        gameOverPanel.SetActive(false);
+        MakeInstance();
+    }
+
+    /** 
+        This function creates a singleton that only exists within the current
+        scene.
+    */
+    void MakeInstance(){
+        if(instance == null){
+            instance = this;
+        }
+    }
+
+    public void PlayAgain(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void BackToMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    /** 
+        This function will disable the active score text, and display the game
+        over panel.
+    */
+    public void GameOverShowPanel(){
+        scoreText.SetActive(false);
+        gameOverPanel.SetActive(true);
+        finalScore.text = "Score\n" + ScoreManager.instance.GetScore(); 
+        gameoverAnim.Play("FadeIn");
+    }
+}
